da7f92d467742fa3343cfced9b8eb1f7a4467a06
tsutrof <jaysonfortus0815@gmail.com>
2023-05-10 15:31:52 +0800

    responsive website

--- dist/js/script.js
+++ dist/js/script.js
@@ -7,3 +7,28 @@ toggleMenu.addEventListener("click", () => {
   headNav.classList.toggle("open");
   blurr.classList.toggle("open");
 });
+
+
+const links = document.querySelectorAll(".links__drop__item h4");
+links.forEach((baba) => {
+  baba.addEventListener("click", () => {
+    baba.nextElementSibling.classList.toggle("open");
+    baba.querySelector("i").classList.toggle("open");
+  });
+});
+
+
+const header1 = document.querySelector(".header");
+const brandImg = document.querySelector(".branding img");
+window.addEventListener("scroll", () => {
+  console.log(window.pageYOffset);
+  if (window.pageYOffset >= 100) {
+    header.classList.add("blue");
+    // brandImg.style="background-color: red;";
+  } else {
+    header.classList.remove("blue");
+    // brandImg.style="background-color: none;";
+    
+    
+  }
+});
